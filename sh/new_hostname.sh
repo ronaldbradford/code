f7cb1b0bda3204bb7b436e943cb87c692bfd6cef
Ronald Bradford <ronald.bradford@gmail.com>
2013-08-20 22:26:23 +0000

    Added New Relic restart

--- sh/new_hostname.sh
+++ sh/new_hostname.sh
@@ -5,11 +5,13 @@
 NEW_HOSTNAME=$1
 [ -z "${NEW_HOSTNAME}" ] && echo "ERROR: $0 <new-hostname>" && exit 1
 
+NEWRELIC="/etc/init.d/newrelic-sysmond"
 if [ ! -z "${REDHAT}" ]
 then
   sudo hostname ${NEW_HOSTNAME}
   sudo sed -i -e "s/^HOSTNAME.*$/HOSTNAME=${NEW_HOSTNAME}/" /etc/sysconfig/network
   sudo service network restart
+  [ -f "${NEWRELIC}" ] && ${NEWRELIC} restart
   hostname
 fi
 
