7d574a3ff69ed19478cd8e99585019d02cbdf7c4
Ronald Bradford <ronald.bradford@techcofounders.com>
2013-10-30 17:17:40 +0000

    Additional pre-requisite checks

--- sh/mysql_tcp_stack.sh
+++ sh/mysql_tcp_stack.sh
@@ -2,12 +2,18 @@
 
 
 [ -z "${DIGESTER}" ] && DIGESTER="pt-query-digest"
-
 [ -z `which ${DIGESTER} 2>/dev/null` ] && echo "'${DIGESTER}' not found. Try wget http://percona.com/get/${DIGESTER}; chmod +x ${DIGESTER}; sudo mv ${DIGESTER} /usr/local/bin" && exit 1
 
+TCPDUMP=/usr/sbin/tcpdump
+[ -z `which ${TCPDUMP} 2>/dev/null` ] && echo "'${TCPDUMP}' not found. Try sudo yum install -y tcpdump" && exit 2
+
+
+# Need a check also for
+# sudo sum install -y perl-Time-HiRes
+
 export DT=`date +%y%m%d.%H%M%S`  
-sudo /usr/sbin/tcpdump -i any port 3306 -s 65535  -x -nn -q -tttt -c 10000 > ${DT}.tcp  
-pt-query-digest --type tcpdump --limit 100%:50 ${DT}.tcp > ${DT}.out
+sudo ${TCPDUMP} -i any port 3306 -s 65535  -x -nn -q -tttt -c 10000 > ${DT}.tcp  
+${DIGESTER} --type tcpdump --limit 100%:50 ${DT}.tcp > ${DT}.out
 gzip ${DT}.tcp &
 more ${DT}.out
 
