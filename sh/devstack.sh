bc753f0b1a4e2e3f536be9c65bcf8c3726c24c27
Ronald Bradford <ronald.bradford@gmail.com>
2015-04-23 18:10:36 -0400

    Changed default interface, set apt-get cache proxy

--- sh/devstack.sh
+++ sh/devstack.sh
@@ -10,23 +10,27 @@ NEW_USER_SUDO_FILE="/etc/sudoers.d/${NEW_USER}"
 [ ! -s ${NEW_USER_SUDO_FILE} ] && umask 226 && echo "${NEW_USER} ALL=(ALL) NOPASSWD: ALL" > ${NEW_USER_SUDO_FILE}
 ls -l ${NEW_USER_SUDO_FILE}
 
+[ -z `which git` ] && sudo apt-get install -y git-core
+
+echo 'Acquire::http::Proxy "http://192.168.1.50:3142";' | tee /etc/apt/apt.conf.d/90-apt-proxy.conf
+
 sudo su - stack -c "git clone https://git.openstack.org/openstack-dev/devstack"
 
 cd /tmp
 LOCAL_CONF="local.conf"
-IP=`/sbin/ifconfig eth0 | grep "inet addr" | cut -d: -f2 | sed -e "s/ .*//"`
+IP=`/sbin/ifconfig eth1 | grep "inet addr" | cut -d: -f2 | sed -e "s/ .*//"`
 
 echo "[[local|localrc]]
 ADMIN_PASSWORD=passwd
 DATABASE_PASSWORD=\$ADMIN_PASSWORD
 RABBIT_PASSWORD=\$ADMIN_PASSWORD
 SERVICE_PASSWORD=\$ADMIN_PASSWORD
-SERVICE_TOKEN=a682f596-76f3-11e3-b3b2-e716f9080d50
+SERVICE_TOKEN=aaaaaaaa-bbbb-cccc-dddd-eeeeeeeeeeee
 HOST_IP=${IP}" > ${LOCAL_CONF}
 
 cat ${LOCAL_CONF}
 
 chown ${NEW_USER}:${NEW_USER} local.conf
-mv ${LOCAL_CONF} ~${NEW_USER}/devstack
+mv ${LOCAL_CONF} /home/${NEW_USER}/devstack
 
 exit 0
