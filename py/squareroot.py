21066403c582e42452c111babf62ea8cc6e9386e
Ronald Bradford <ronald.bradford@gmail.com>
2015-05-06 09:44:36 -0400

    A better square root

---
+++ py/squareroot.py
@@ -0,0 +1,9 @@
+def squareroot(number, precision = 5):
+  root = number/2.0
+  for i in range(20):
+    nroot = (1/2.0)*(root + (number / root))
+    #print i, nroot
+    if (root - nroot < 1.0/10**precision):
+      break
+    root = nroot
+  return round(nroot, precision)
