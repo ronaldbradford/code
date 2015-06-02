598bb654274585cf006cf1700fc1b2e80d363527
Ronald Bradford <ronald.bradford@gmail.com>
2015-06-02 16:37:09 -0400

    Added necessary packages to a clean Ubuntu install to work in OpenStack development

---
+++ scripts/bootstrap_openstack_dev.sh
@@ -0,0 +1,2 @@
+sudo apt-get install -y git-core git-review python-tox python-dev python3-dev python-setuptools
+sudo apt-get install -y libffi-dev libssl-dev libldap2-dev libffi-dev libsasl2-dev libxslt1-dev libxml2-dev
